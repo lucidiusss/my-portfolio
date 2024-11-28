e440c7a00a9c68aa1ca9d4b525121d639b5b67f4
lucidiusss <cheapp78@gmail.com>
2024-11-28 10:05:01 +0300

    upd

--- app/(primary)/page.tsx
+++ app/(primary)/page.tsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from "react";
 
 import About from "../components/About";
-import Footer from "../components/Footer";
 import Main from "../components/Main";
 
 const LazyFooter = lazy(() => import("../components/Footer"));
